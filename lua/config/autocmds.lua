bc8722ff4a6be59f2f72c0b720c3c81aad4ba702
Kristian Klette <geferg.dev@gmail.com>
2025-05-09 00:14:19 +0200

    feat: add core options and autocmds

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,60 @@
+-- Prevent comment continuation
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200 })
+  end,
+})
+
+-- Trim trailing whitespace
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
+-- Auto reload file when changed externally
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  pattern = "*",
+  command = "checktime",
+})
+
+-- Auto create nested directories
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local dir = vim.fn.expand("<afile>:p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
+
+-- Propper filetypes for uncommon extensions
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.cconf",
+  command = "set filetype=conf",
+})
+
+
+-- Restore last cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local mark = vim.fn.line([['"]])
+    if mark > 1 and mark <= vim.fn.line("$") then
+      vim.cmd('normal! g`"')
+    end
+  end,
+})
---
+++ lua/config/options.lua
@@ -0,0 +1,44 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+
+vim.g.netrw_liststyle = 0
+vim.g.netrw_bufsettings = "noma nomod nobl nowrap ro"
+vim.g.netrw_banner = 0
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.autoindent = true
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+
+vim.opt.clipboard:append("unnamedplus")
+
+vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
+vim.opt.undofile = true
+
+vim.opt.signcolumn = "yes"
+vim.opt.backspace = { "indent", "eol", "start" }
+
+vim.opt.scrolloff = 8
+vim.opt.isfname:append("@-@")
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.showcmd = true
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.updatetime = 250
