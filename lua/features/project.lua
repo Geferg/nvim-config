6424fdcbddf1c709e5ed194bc83c876e612be138
Kristian Klette <geferg.dev@gmail.com>
2025-05-09 19:24:49 +0200

    chore: reformat and rework which-key structure

--- lua/features/project.lua
+++ lua/features/project.lua
@@ -1,20 +1,21 @@
 local M = {}
 local project_root = nil
 
+-- Set current file's folder as project root
 function M.set_project_dir()
-  project_root = vim.fn.expand("%:p:h")
-  vim.cmd.cd(project_root)
-  print("Project directory set to: " .. project_root)
+    project_root = vim.fn.expand("%:p:h")
+    vim.cmd.cd(project_root)
+    print("Project directory set to: " .. project_root)
 end
 
 function M.view_project_dir()
-  if project_root then
-    vim.cmd.cd(project_root)
-    vim.cmd.Ex()
-  else
-    vim.cmd.Ex()
-    vim.notify("No project directory set — opened file's directory instead", vim.log.levels.WARN)
-  end
+    if project_root then
+        vim.cmd.cd(project_root)
+        vim.cmd.Ex()
+    else
+        vim.cmd.Ex()
+        vim.notify("No project dir set — opened file's directory instead", vim.log.levels.WARN)
+    end
 end
 
 return M
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,22 +1,33 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {}, -- Required to suppress Lazy's warning if using `config`
-  config = function()
-    local wk = require("which-key")
-    local project = require("features.project")
-    local files = require("features.files")
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {}, -- Required to suppress Lazy's warning if using `config`
+    config = function()
+        local wk = require("which-key")
+        local project = require("features.project")
+        local files = require("features.files")
 
-    wk.register({
-      -- Group headings
-      ["<leader>f"] = { name = "Files" },
-      ["<leader>p"] = { name = "Project" },
+        wk.register({
+            -- Group headings
+            ["<leader>p"] = {
+                name = "Project",
+                v = { project.view_project_dir, "View project directory" },
+                s = { project.set_project_dir, "Set project directory" },
+            },
 
-      -- Actual mappings
-      ["<leader>fe"] = { files.exit_file, "Close file" },
-      ["<leader>ps"] = { project.set_project_dir, "Set project directory" },
-      ["<leader>pv"] = { project.view_project_dir, "View project directory" },
-    }, { mode = "n" })
-  end,
+            ["<leader>f"] = { name = "Files" },
+            ["<leader>d"] = { name = "Directory" },
+            ["<leader>g"] = { name = "Git" },
+            ["<leader>l"] = { name = "LSP" },
+            ["<leader>b"] = { name = "Build" },
+            ["<leader>s"] = { name = "Splits" },
+            ["<leader>w"] = { name = "Window" },
+            ["<leader>t"] = { name = "Tools" },
+
+            ["<leader>fe"] = { vim.cmd.Ex, "Close file" },
+
+        }, { mode = "n" })
+    end,
 }
+
 
