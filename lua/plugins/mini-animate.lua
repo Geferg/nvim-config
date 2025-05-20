9da7efde63929b992807e69cf4a27885b86d3c42
Kristian Klette <geferg.dev@gmail.com>
2025-05-20 20:36:54 +0200

    feat: added smooth scrolling

---
+++ lua/plugins/mini-animate.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "echasnovski/mini.animate",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("mini.animate").setup({
+        cursor = {
+          enable = true,
+          timing = require("mini.animate").gen_timing.linear({ duration = 70, unit = "total" }),
+        },
+        scroll = {
+          enable = true,
+          timing = require("mini.animate").gen_timing.linear({ duration = 150, unit = "total" }),
+        },
+        resize = {
+          enable = true,
+          timing = require("mini.animate").gen_timing.linear({ duration = 120, unit = "total" }),
+        },
+        open = { enable = false }, -- Optional: don't animate splits/popups
+        close = { enable = false },
+      })
+    end,
+  },
+}
