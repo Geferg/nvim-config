aa109ad03e1bc50cb88da07d5815f7c823052a05
Kristian Klette <geferg.dev@gmail.com>
2025-07-14 01:06:44 +0200

    fix: removed plugin messing with signcolumn

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -117,18 +117,35 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 -- Focus ignores
-local augroup =
-    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+-- local augroup =
+--     vim.api.nvim_create_augroup('FocusDisable', { clear = true })
 
-local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
-vim.api.nvim_create_autocmd('WinEnter', {
-    group = augroup,
-    callback = function(_)
-        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
-        then
-            vim.w.focus_disable = true
-        else
-            vim.w.focus_disable = false
+-- local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+-- vim.api.nvim_create_autocmd('WinEnter', {
+--     group = augroup,
+--     callback = function(_)
+--         if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+--         then
+--             vim.w.focus_disable = true
+--         else
+--             vim.w.focus_disable = false
+--         end
+--     end,
+--     desc = 'Disable focus autoresize for BufType',
+-- })
+--
+-- local ignore_filetypes = { 'neo-tree' }
+-- vim.api.nvim_create_autocmd('FileType', {
+--     group = augroup,
+--     callback = function(_)
+--         if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+--             vim.b.focus_disable = true
+--         else
+--             vim.b.focus_disable = false
+--         end
+--     end,
+--     desc = 'Disable focus autoresize for FileType',
+-- })
         end
     end,
     desc = 'Disable focus autoresize for BufType',
