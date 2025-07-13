bf703480e862634fefa5c9700339d82aabb84d39
Kristian Klette <geferg.dev@gmail.com>
2025-07-13 15:54:37 +0200

    feat: added split autofocus plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -115,3 +115,34 @@ vim.api.nvim_create_autocmd("LspAttach", {
         --print("LspAttach triggered for client " .. vim.lsp.get_client_by_id(args.data.client_id).name)
     end,
 })
+
+-- Focus ignores
+local augroup =
+    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+vim.api.nvim_create_autocmd('WinEnter', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+        then
+            vim.w.focus_disable = true
+        else
+            vim.w.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for BufType',
+})
+
+local ignore_filetypes = { 'neo-tree' }
+vim.api.nvim_create_autocmd('FileType', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.b.focus_disable = true
+        else
+            vim.b.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for FileType',
+})
