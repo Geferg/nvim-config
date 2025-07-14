343a9b4c5eb53ad8cc096dc3b2bb22f99193ef12
Kristian Klette <geferg.dev@gmail.com>
2025-07-14 16:17:53 +0200

    chore: changed json viewer appearance

--- lua/plugins/nvim-json-graph-view.lua
+++ lua/plugins/nvim-json-graph-view.lua
@@ -2,7 +2,8 @@ return {
     {
         "Owen-Dechow/nvim_json_graph_view",
         opts = {
-            round_units = false
+            round_units = true,
+            editor_type = "floating",
         }
     }
 }
