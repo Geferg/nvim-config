a29e991bd7a1ab0d84db5cd5b29d2fa83507f879
Kristian Klette <geferg.dev@gmail.com>
2025-07-14 16:29:01 +0200

    chore: replaced deprecated spec for neotree navigation

--- lua/features/misc.lua
+++ lua/features/misc.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.conditional_dash()
     local ft = vim.bo.filetype
     if ft == "neo-tree" or ft == "NvimTree" then
-        require("features.files").go_up_dir_in_place()
+        require("features.files").up_one()
     else
         -- fallback to default behavior: move up a line and to first non-blank char
         local count = vim.v.count > 0 and tostring(vim.v.count) or ""
