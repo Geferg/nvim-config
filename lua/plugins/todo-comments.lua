758942be57d9087f989a475e95d66d6114f8718f
Kristian Klette <geferg.dev@gmail.com>
2025-07-18 01:05:58 +0200

    feat: added todo comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        -- TODO:
+        -- FIX:
+        -- HACK:
+        -- NOTE:
+        -- PERF:
+        -- WARNING:
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+    }
+}
