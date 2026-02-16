e6edd59a6a9fd737adcff5dc9b49df1ad27703a6
Kristian Klette <geferg.dev@gmail.com>
2026-02-16 23:19:35 +0100

    refactor: remove explicit lsp enable

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,49 +1,34 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-        "pyright",
-        "cmake",
-        "bashls",
-        "clangd",
-        "html",
-        "cssls",
-        "jsonls",
-        "eslint",
-        "tailwindcss",
-        "ts_ls",
-    },
-    automatic_enable = true, -- default, but explicit never hurts
+	ensure_installed = {
+		"lua_ls",
+		"rust_analyzer",
+		"pyright",
+		"cmake",
+		"bashls",
+		"clangd",
+		"html",
+		"cssls",
+		"jsonls",
+		"eslint",
+		"tailwindcss",
+		"ts_ls",
+	},
+	automatic_enable = true, -- default, but explicit never hurts
 })
 
 local caps = vim.lsp.protocol.make_client_capabilities()
 local ok, cmp = pcall(require, "cmp_nvim_lsp")
 if ok then
-    caps = cmp.default_capabilities(caps)
+	caps = cmp.default_capabilities(caps)
 end
 
 local function on_attach(_, bufnr)
-    -- example keymap
-    -- vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+	-- example keymap
+	-- vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
 end
 
 vim.lsp.config("*", {
-    capabilities = caps,
-    on_attach    = on_attach,
-})
-
-vim.lsp.enable({
-    "lua_ls",
-    "rust_analyzer",
-    "pyright",
-    "cmake",
-    "bashls",
-    "clangd",
-    "html",
-    "cssls",
-    "jsonls",
-    "eslint",
-    "tailwindcss",
-    "ts_ls",
+	capabilities = caps,
+	on_attach = on_attach,
 })
